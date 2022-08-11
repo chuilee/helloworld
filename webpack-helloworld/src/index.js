5ab2ebb22b03ab4142d6dfaf6a7f96c155eb8b77
Chuilee <chui.lee8@gmail.com>
2022-08-12 00:02:19 +0800

    chore:资源管理

--- webpack-helloworld/src/index.js
+++ webpack-helloworld/src/index.js
@@ -1,10 +1,17 @@
 import _ from "lodash";
+import Image1 from './images/image1.webp';
+import "./styles/main.scss";
 
 function component() {
   let element = document.createElement("div");
-
   // lodash（目前通过一个 script 引入）对于执行这一行是必需的
   element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.classList.add("blue");
+
+  let image1 = document.createElement('img')
+  image1.src = Image1;
+
+  element.appendChild(image1);
 
   return element;
 }
--- webpack-helloworld/webpack.config.js
+++ webpack-helloworld/webpack.config.js
@@ -1,9 +1,54 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const devMode = process.env.NODE_ENV !== "production";
 
 module.exports = {
+  mode: "production",
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
+  module: {
+    rules: [
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          devMode ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|svg|jpe?g|gif|webp)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 6192,
+            },
+          },
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true, // webpack@1.x
+              disable: true, // webpack@2.x and newer
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: devMode ? "[name].css" : "[name].[hash].css",
+      chunkFilename: devMode ? "[id].css" : "[id].[hash].css",
+    }),
+  ],
 };
